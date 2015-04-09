wikimedia/service-runner

commit f5986f5a1e941c11cb21d623e30c82dd223b8fca

Set txstatsd option to false by default
Wikimedia is migrating from txStatsD to statsite, so the txstatsd option should be false by default.
See [T94053](https://phabricator.wikimedia.org/T94053).

lib/statsd.js
@@ -44,7 +44,7 @@ function makeStatsD(options, logger) {
         port: options.port,
         prefix:  srvName + '.',
         suffix: '',
-        txstatsd  : true,
+        txstatsd  : false,
         globalize : false,
         cacheDns  : true,
         mock      : false

commit a4b385e3a585799a4b8459934817add9ef58fcbf

Fix txstatsd support & release v0.1.7
f5986f5a1e9 broke txstatsd support, and this patch basically reverts it. The
txtstats option was always ignored by the statsd backend library, but in this
patch we are also setting it to false to make things clearer.

lib/statsd.js
@@ -44,10 +44,10 @@ function makeStatsD(options, logger) {
         port: options.port,
         prefix:  srvName + '.',
         suffix: '',
-        txstatsd  : false,
-        globalize : false,
-        cacheDns  : true,
-        mock      : false
+        txstatsd: options.type === 'txstatsd',
+        globalize: false,
+        cacheDns: true,
+        mock: false
     };
     if (options.type === 'txstatsd') {
         statsd = new TXStatsD(statsdOptions);